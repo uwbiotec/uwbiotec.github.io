ce0ad25eead1b241c5e7f731070e5797933eeebb
muizsohani <46656624+muizsohani@users.noreply.github.com>
2019-09-30 16:44:52 -0400

    Updated Speakers Section
    Bios added + Design changes

--- script.js
+++ script.js
@@ -56,25 +56,76 @@ function setContent(){
 
 function showBio(div){
 	var bios = document.getElementsByClassName("speaker-bio-wrapper");
+	var openArrow = document.getElementsByClassName("arrow-wrapper-open");
 	var i;
 
 	for(i=0; i<bios.length; i++){
 		bios[i].classList.remove('open')
+		openArrow[i].classList.remove('hide');
 	}
 
 	div.nextSibling.nextSibling.classList.toggle("open");
+	div.childNodes[3].childNodes[5].classList.toggle("hide");
+	
+}
 
+function showBio1(div){
+	var biosCol1 = document.getElementsByClassName("speaker-bio-wrapper-col1");
+	var i;
+
+	for(i=0; i<biosCol1.length; i++){
+		biosCol1[i].classList.remove('open')
+	}
+
+	div.nextSibling.nextSibling.classList.toggle("open");
+}
+
+function showBio2(div){
+	var biosCol2 = document.getElementsByClassName("speaker-bio-wrapper-col2");
+	var i;
+
+	for(i=0; i<biosCol2.length; i++){
+		biosCol2[i].classList.remove('open')
+	}
+
+	div.nextSibling.nextSibling.classList.toggle("open");
 }
 
 window.onclick = function(event){
 
 	if(!event.target.matches('.speaker-wrapper')){
 		var bios = document.getElementsByClassName("speaker-bio-wrapper");
+		var openArrow = document.getElementsByClassName("arrow-wrapper-open");
 		var i;
 		for(i=0; i<bios.length; i++){
-			if (bios[i].classList.contains('open')){
+			if (bios[i].classList.contains('open') && openArrow[i].classList.contains('hide')){
 				bios[i].classList.remove('open');
+				openArrow[i].classList.remove('hide');
 			}
 		}
 	}
+
+	if(event.target.matches('.arrow-wrapper-col1')){
+		var biosCol1 = document.getElementsByClassName("speaker-bio-wrapper-col1");
+		var i;
+		
+		for(i=0; i<biosCol1.length; i++){
+			if (biosCol1[i].classList.contains('open')){
+				biosCol1[i].classList.remove('open');
+			}
+		}
+	}
+
+	if(event.target.matches('.arrow-wrapper-col2')){
+		var biosCol2 = document.getElementsByClassName("speaker-bio-wrapper-col2"); 
+		var i;
+
+		for(i=0; i<biosCol2.length; i++){
+			if (biosCol2[i].classList.contains('open')){
+				biosCol2[i].classList.remove('open');
+			}
+		}
+	}
+
+
 }
